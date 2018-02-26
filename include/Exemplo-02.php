6b996eac08064e2e2f7f369d3cdf5e4dc28c9f47
Guilherme Ribeiro <guilherme.filipe.ribeiro@gmail.com>
2018-02-26 16:12:59 +0000

    Novos Exemplos

--- include/Exemplo-02.php
+++ include/Exemplo-02.php
@@ -1,6 +1,9 @@
 <?php
 
-include "inc/Exemplo-01.php";
+//include "inc/Exemplo-01.php"; tenta funcionar mesmo que o ficheiro não exista ou que esteja com erros inclupde path e include dinamico(pessima pratica)
+
+require_once "inc/Exemplo-01.php"; //obriga que o ficheiro exista e que esteja a funcionar corretamente senão gera um erro fatal
+require_once "inc/Exemplo-01.php";
 
 $resultado = somar(10, 20);
 
