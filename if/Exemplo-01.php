68743d04c719b881a4292e77609971d2164fb3d2
Guilherme Ribeiro <guilherme.filipe.ribeiro@gmail.com>
2018-02-26 17:33:17 +0000

    Adicionados novos Exemplos

---
+++ if/Exemplo-01.php
@@ -0,0 +1,33 @@
+<?php
+
+$qualASuaIdade = 125;
+
+$idadeCrianca = 12;
+
+$idadeMaior = 18;
+
+$idadeMelhor = 65;
+
+if ($qualASuaIdade < $idadeCrianca) {
+
+	echo "Criança";
+
+} elseif ($qualASuaIdade < $idadeMaior) {
+
+	echo "Adolescente";
+
+} elseif ($qualASuaIdade < $idadeMelhor){
+
+	echo "Adulto";
+}
+else {
+
+	echo "Idoso";
+
+}
+
+echo "</br>";
+
+echo ($qualASuaIdade < $idadeMaior)?"Menor de Idade":"Maior de Idade";
+
+?>
