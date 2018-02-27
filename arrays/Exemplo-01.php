c2df7d168e69241263507efd1a5251c475d4ad8c
Guilherme Ribeiro <guilherme.filipe.ribeiro@gmail.com>
2018-02-27 11:58:22 +0000

    Novo Exemplo

---
+++ arrays/Exemplo-01.php
@@ -0,0 +1,8 @@
+<?php
+
+$frutas = array("Maça","Laranja","Abacaxi","Melancia"); //uma dimensão chamda vector
+
+print_r($frutas) 
+
+?>
+
---
+++ arrays/Exemplo-03.php
@@ -0,0 +1,18 @@
+<?php
+
+$pessoas = array();
+
+array_push($pessoas, array(
+	'nome'=>'Afonso',
+	'idade'=>6
+));
+
+array_push($pessoas, array(
+	'nome'=>'Madalena',
+	'idade'=>2
+
+));
+
+print_r($pessoas[0]);
+
+?>
