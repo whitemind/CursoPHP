7d10453ebef177151e36d34c852ecd1e0a80a945
Guilherme Ribeiro <guilherme.filipe.ribeiro@gmail.com>
2018-02-27 19:21:10 +0000

    Novos Exemplos

---
+++ funcoesutilizador/Exemplo-01.php
@@ -0,0 +1,15 @@
+<?php 
+
+function ola() {
+
+	return "Olá Mundo!<br>";
+
+}
+
+echo ola();
+
+$frase = ola();
+
+echo strlen($frase);
+
+ ?>
---
+++ funcoesutilizador/Exemplo-02.php
@@ -0,0 +1,10 @@
+<?php 
+
+function salario() {
+
+	return 946.00;
+}
+
+echo "Recebeu 3 salários: ".(salario()*3);
+
+ ?>
