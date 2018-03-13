64a7841342d62dcb2235e3950b2008978d15fadc
Guilherme Ribeiro <guilherme.filipe.ribeiro@gmail.com>
2018-03-13 14:12:19 +0000

    Novos Exemplos

---
+++ dao/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+require_once("config.php");
+
+//$sql = new Sql();
+
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+//echo json_encode($usuarios);
+
+$root = new Usuario();
+
+$root->loadById(1);
+
+echo $root;
+
+ ?>
