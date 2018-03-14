22d8e2b8dbf0c9b5ffd9c85c630e98648a4b31f9
Guilherme Ribeiro <guilherme.filipe.ribeiro@gmail.com>
2018-03-14 10:58:15 +0000

    Novos Exemplos

---
+++ dao/index.php
@@ -0,0 +1,40 @@
+<?php 
+
+require_once("config.php");
+
+//$sql = new Sql();
+
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+//echo json_encode($usuarios);
+
+//Visualiza um usuario
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
+
+//Lista todos os usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//carrega uma lista de usuarios pesquisa por Login
+//$search = Usuario::search("Bru");
+//echo json_encode($search);
+
+//Carrega um usuario usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root","123456");
+
+//echo $usuario;
+
+$aluno = new Usuario("Bruno Amaro","654897");
+
+//$aluno->setDeslogin("aluno");
+//$aluno->setDessenha("123987");
+
+$aluno->insert();
+
+
+echo $aluno;
+
+ ?>
