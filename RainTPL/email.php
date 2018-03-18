3b7749a7f75de86288e4e2229bdb9996a5a01be5
Guilherme Ribeiro <guilherme.filipe.ribeiro@gmail.com>
2018-03-18 15:39:21 +0000

    Novos Exemplos

--- RainTPL/email.php
+++ RainTPL/email.php
@@ -2,6 +2,31 @@
 
 require_once("vendor/autoload.php");
 
+// namespace
+use Rain\Tpl;
+
+// config
+$config = array(
+    "tpl_dir"       => "tpl/",
+    "cache_dir"     => "cache/",
+    "debug"         => false // set to false to improve the speed
+);
+Tpl::configure( $config );
+// Add PathReplace plugin (necessary to load the CSS with path replace)
+//Tpl::registerPlugin( new Tpl\Plugin\PathReplace() );
+// create the Tpl object
+$tpl = new Tpl;
+// assign a variable
+
+$tpl->assign( "name", "Obi Wan Kenoby" );
+
+$tpl->assign( "version", PHP_VERSION );
+// assign an array
+//$tpl->assign( "week", array( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ) );
+
+// draw the template
+$html = $tpl->draw( "index",true );
+
 //Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
 
@@ -52,7 +77,7 @@ $mail->Subject = 'PHPMailer GMail SMTP test';
 
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+$mail->msgHTML($html);
 
 //Replace the plain text body with one created manually
 $mail->AltBody = 'Envio de email do curso de PHP 7.';
@@ -70,25 +95,6 @@ if (!$mail->send()) {
     #if (save_mail($mail)) {
     #    echo "Message saved!";
     #}
-}
-
-//Section 2: IMAP
-//IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
-//Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
-//You can use imap_getmailboxes($imapStream, '/imap/ssl') to get a list of available folders or labels, this can
-//be useful if you are trying to get this working on a non-Gmail IMAP server.
-function save_mail($mail)
-{
-    //You can change 'Sent Mail' to any other folder or tag
-    $path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
-
-    //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
-    $imapStream = imap_open($path, $mail->Username, $mail->Password);
-
-    $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-    imap_close($imapStream);
-
-    return $result;
 }
 
  ?>
