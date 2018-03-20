14ec6dbaa04af2ba5c164234a5356b02bcf0a10d
Guilherme Ribeiro <guilherme.filipe.ribeiro@gmail.com>
2018-03-20 19:34:23 +0000

    Criada a classe Page

--- Projeto/101 - Classe Page para o Template/index.php
+++ Projeto/101 - Classe Page para o Template/index.php
@@ -2,16 +2,13 @@
 
 require_once("vendor/autoload.php");
 
-use \Slim\Slim;
-use \Hcode\Page;
-
-$app = new Slim();
+$app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-
-	$page = new Page();
+    
+	$page = new Hcode\Page();
 
 	$page->setTpl("index");
 
