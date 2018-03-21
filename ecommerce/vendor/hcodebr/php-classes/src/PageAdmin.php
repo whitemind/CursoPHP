cf1f8ea089045fe19d03f09370f2df7050797b3c
Guilherme Ribeiro <guilherme.filipe.ribeiro@gmail.com>
2018-03-21 12:46:40 +0000

    Classe Page já a funcionar

--- ecommerce/vendor/hcodebr/php-classes/src/PageAdmin.php
+++ ecommerce/vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -3,10 +3,10 @@
 namespace Hcode;
 
 class PageAdmin extends Page {
-															 
+															 	
 	public function __construct($opts = array(), $tpl_dir = "C:/xampp/htdocs/CursoPHP/ecommerce/views/admin/")
 	{ 
-		echo $tpl_dir;
+		
 		parent::__construct($opts, $tpl_dir);
 
 	}
