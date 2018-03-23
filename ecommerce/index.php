ffbb9f657aac262f4b4afe5dc325a6c5b3ed9522
Guilherme Ribeiro <guilherme.filipe.ribeiro@gmail.com>
2018-03-23 18:29:25 +0000

    Utilizadores - CRUD

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -95,6 +95,15 @@ $app->get('/admin/users/:iduser/delete', function($iduser)
 
 	User::verifyLogin();
 
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
 });
 
 $app->get('/admin/users/:iduser', function($iduser) 
@@ -147,22 +156,6 @@ $app->post('/admin/users/:iduser', function($iduser)
 	$user->setData($_POST);
 
 	$user->update();
-
-	header("Location: /admin/users");
-	exit;
-
-});
-
-$app->get('/admin/users/:iduser/delete', function($iduser) 
-{
-
-	User::verifyLogin();
-
-	$user = new User();
-
-	$user->get((int)$iduser);
-
-	$user->delete();
 
 	header("Location: /admin/users");
 	exit;
