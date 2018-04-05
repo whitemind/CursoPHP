efbce3eae7c6704429aa906dac0af579b43e937e
Guilherme Ribeiro <guilherme.filipe.ribeiro@gmail.com>
2018-04-05 18:01:14 +0100

    Admin - Categorias CRUD

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -7,6 +7,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -232,6 +233,18 @@ $app->post("/admin/forgot/reset", function(){
 	]);
 
 	$page->setTpl("forgot-reset-success");
+
+});
+
+$app->get("/admin/categories", function() {
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		"cagetories"=>$categories
+	]);
 
 });
 
